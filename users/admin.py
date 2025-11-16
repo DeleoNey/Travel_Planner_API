a190832d069742b4b231f81cec0abf9174055c82
Deleon <makslesk64@gmail.com>
2025-11-16 21:53:50 +0200

    Add UserSerializer, RegisterView/ProfileView, JWT Auth, Register User in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from users.models import User
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets
